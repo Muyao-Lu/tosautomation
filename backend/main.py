e170e2361dde043bad95fd4ee5041ccf282f12e4
Muyao Lu <130-MLU@sd43.bc.ca>
2025-06-25 20:59:26 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -26,36 +26,36 @@ class Request(BaseModel):
 
 @app.post("/{document_type}/")
 async def process_terms_of_service(document_type, request: Request):
-    return {"hi":"hi"}
-    """""
-    if ip_validation.check_request_time_validity(request.ip):
-        if document_type != "followup":
-            return "success"
-            webscraper.scrape_to_db(request.link)
-            website = webscraper.get_full_website()
-            text = ai_api.call_summarizer(short=request.short, policy=website, language_level=request.lang)
-            return convert_to_html(text)
-        else:
-
-            if request.query is not None:
-                print("1")
-                try:
-                    text = ai_api.chat_completion(link=request.link, query=request.query)
-                    print("2")
-                    return convert_to_html(text)
-                except NoResultFound:
-                    webscraper.scrape_to_db(request.link)
-                    text = ai_api.chat_completion(link=request.link, query=request.query)
-                    print("3")
-                    return convert_to_html(text)
-
+    try:
+        if ip_validation.check_request_time_validity(request.ip):
+            if document_type != "followup":
+                return "success"
+                webscraper.scrape_to_db(request.link)
+                website = webscraper.get_full_website()
+                text = ai_api.call_summarizer(short=request.short, policy=website, language_level=request.lang)
+                return convert_to_html(text)
             else:
-                print("4")
-                raise HTTPException(status_code=400, detail="Query parameter cannot be empty for followup questions. Please provide a query in the request body.")
-    else:
-        print("5")
-        raise HTTPException(status_code=429, detail="Too many requests. Please obey the 20s rule between requests")
-    """""
+    
+                if request.query is not None:
+                    print("1")
+                    try:
+                        text = ai_api.chat_completion(link=request.link, query=request.query)
+                        print("2")
+                        return convert_to_html(text)
+                    except NoResultFound:
+                        webscraper.scrape_to_db(request.link)
+                        text = ai_api.chat_completion(link=request.link, query=request.query)
+                        print("3")
+                        return convert_to_html(text)
+    
+                else:
+                    print("4")
+                    raise HTTPException(status_code=400, detail="Query parameter cannot be empty for followup questions. Please provide a query in the request body.")
+        else:
+            print("5")
+            raise HTTPException(status_code=429, detail="Too many requests. Please obey the 20s rule between requests")
+    except Exception as e:
+        return {"error": e}
 
 
 if __name__ == '__main__':
