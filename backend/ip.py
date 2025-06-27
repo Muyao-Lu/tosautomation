2d6d145da3e0fefcc3ff68c889b473157753930f
Muyao Lu <130-MLU@sd43.bc.ca>
2025-06-26 19:45:16 -0700

    Chore: Migrated to Supabase

---
+++ backend/ip.py
@@ -0,0 +1,62 @@
+import datetime
+from sqlmodel import SQLModel, Field, create_engine, select, Session
+from sqlalchemy.exc import NoResultFound
+import os
+
+class IpModel(SQLModel, table=True):
+    id: int | None = Field(None, primary_key=True)
+    ip: str = Field(None, index=True)
+    last_call: datetime.datetime
+
+class IpController:
+    def __init__(self, time_limit: float | int = 20):
+        self.time_limit = time_limit
+        self.DATABASE_URL = "postgresql://postgres:{key}@db.kvzyyqxeawybeukfybkq.supabase.co:5432/postgres".format(key=os.environ["SUPABASE_KEY"])
+        self.engine = create_engine(self.DATABASE_URL)
+        SQLModel.metadata.create_all(self.engine)
+
+    def add_ip(self, ip):
+        """
+            Adds an ip and time to the database
+        """
+        with Session(self.engine) as session:
+            try:
+                self.get_ip(ip)
+            except NoResultFound:
+                session.add(IpModel(ip=ip, last_call=datetime.datetime.now()))
+                session.commit()
+
+    def get_ip(self, ip):
+        """
+           Gets the row with the corresponding ip from the database
+        """
+        with Session(self.engine) as session:
+            condition = select(IpModel).where(IpModel.ip == ip)
+            results = session.exec(condition)
+            return results.one()
+
+    def update_ip(self, ip):
+        """
+            Updates IP information
+        """
+        with Session(self.engine) as session:
+            entry = self.get_ip(ip)
+            entry.last_call = datetime.datetime.now()
+            session.add(entry)
+            session.commit()
+
+    def check_request_time_validity(self, ip):
+        """
+            Checks time validity of an API Request
+        """
+        try:
+            entry = self.get_ip(ip)
+            if (datetime.datetime.now() - entry.last_call).total_seconds() > self.time_limit:
+                self.update_ip(ip)
+                return True
+            else:
+                return False
+        except NoResultFound:
+            self.add_ip(ip)
+            return True
+
---
+++ backend/vector_db.py
@@ -0,0 +1,61 @@
+from sqlmodel import SQLModel, Field, create_engine, select, Session
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from sqlalchemy.exc import NoResultFound
+import requests, os
+
+
+class VectorDatabaseModel(SQLModel, table=True):
+    id: int | None = Field(None, primary_key=True)
+    link: str = Field(index=True)
+    document: str
+
+class VectorDatabaseControl:
+    def __init__(self, top_k=1):
+        self.DATABASE_URL = "postgresql://postgres:{key}@db.fkbsrnqotuynrsplskxe.supabase.co:5432/postgres".format(key=os.environ["SUPABASE_KEY"])
+        self.engine = create_engine(self.DATABASE_URL)
+        self.text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=2000,
+            chunk_overlap=200
+        )
+
+        self.URL = "https://tosautomation-embeddings.vercel.app"
+        SQLModel.metadata.create_all(self.engine)
+
+    def add(self, item, link):
+        with Session(self.engine) as session:
+            try:
+                self.get_by_link(link)
+            except NoResultFound:
+                session.add(VectorDatabaseModel(link=link, document=item))
+                session.commit()
+
+
+    def get_by_link(self, link):
+        with Session(self.engine) as session:
+            condition = select(VectorDatabaseModel).where(VectorDatabaseModel.link == link)
+            results = session.exec(condition)
+
+            return results.one()
+
+
+    def get_closest_neighbor(self, link, query):
+        try:
+            item = self.get_by_link(link)
+            all_sections = self.text_splitter.split_text(item.document)
+            headers = {
+                'Content-Type': 'application/json',
+            }
+
+            json_data = {
+                "query": query,
+                "documents": all_sections
+            }
+
+            response = requests.post(self.URL, headers=headers, json=json_data)
+            return response.json()[1]
+
+        except NoResultFound:
+            return None
+
+vector_db = VectorDatabaseControl()
+vector_db.add("hello", "test.com")
