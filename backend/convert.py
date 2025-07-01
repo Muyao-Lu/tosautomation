47bdc1f6facf53a6edde92bec30c2d81c36e7719
Muyao Lu <130-MLU@sd43.bc.ca>
2025-07-01 09:24:10 -0700

    Fix: Made convert.py actually work
    Just found out the the replace method returns a new string, instead of assigning the old string the new value.
    
    I HAVE A BRAIN?! RIGHT???

--- backend/convert.py
+++ backend/convert.py
@@ -2,7 +2,7 @@ import markdown
 
 def convert_to_html(mk):
     converted = markdown.markdown(mk)
-    converted.replace("\n", "<br>")
-    converted.replace("\"", " ")
+    converted = converted.replace("\n", "<br>")
+    converted = converted.replace("\"", " ")
     return converted
 
