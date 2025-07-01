5774d62b42e2f1bfa613e8e7c3a1bac96ff55ac2
Muyao Lu <130-MLU@sd43.bc.ca>
2025-07-01 12:37:10 -0700

    Fix: Make code work this time
    i promise

--- backend/convert.py
+++ backend/convert.py
@@ -2,8 +2,8 @@ import markdown
 
 def convert_to_html(mk):
     converted = markdown.markdown(mk)
-    converted.replace("\n", "<br>")
-    converted.replace('"', " ")
+    converted = converted.replace("\n", "<br>")
+    conterted = converted.replace('"', " ")
     return converted
 
 
