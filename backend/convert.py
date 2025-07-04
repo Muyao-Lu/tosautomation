3853134aceb50e5a274661a02a5ccd7feec36b2c
Muyao Lu <130-MLU@sd43.bc.ca>
2025-07-03 21:13:13 -0700

    Feat: Link validation using regex

--- backend/convert.py
+++ backend/convert.py
@@ -1,9 +1,15 @@
 import markdown
+import re
 
 def convert_to_html(mk):
     converted = markdown.markdown(mk)
-    converted = converted.replace("\n", "<br>")
-    conterted = converted.replace('"', " ")
+    converted.replace("\n", "<br>")
+    converted.replace('"', " ")
     return converted
+
+def check_link(link):
+    link_pattern = "(https://|http://)(www.|)+[a-zA-Z0-9-]+\.+[a-zA-Z]*+[a-zA-Z0-9/-]*" # Regex. AAAAAAHHHH
+    return re.search(string=link, pattern=link_pattern)
+
 
 
--- backend/main.py
+++ backend/main.py
@@ -3,20 +3,17 @@ from fastapi.exceptions import HTTPException
 import uvicorn
 from pydantic import BaseModel
 from sqlalchemy.exc import NoResultFound
-from fastapi.middleware.cors import CORSMiddleware
 
 from ai import AiAccess
 from scraper import ScraperDatabaseControl
-from convert import convert_to_html
+from convert import convert_to_html, check_link
 from ip import IpController
-
 
 app = FastAPI()
 
 webscraper = ScraperDatabaseControl()
 ai_api = AiAccess()
 ip_validation = IpController()
-print("0.5")
 class Request(BaseModel):
     link: str
     ip: str
@@ -24,51 +21,36 @@ class Request(BaseModel):
     short: bool = False
     query : str = None
 
-origins = [
-    "http://localhost:63342"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
-
 
 @app.post("/{document_type}/")
 async def process_terms_of_service(document_type, request: Request):
-    try:
+    if check_link(request.link):
         if ip_validation.check_request_time_validity(request.ip):
             if document_type != "followup":
+
                 webscraper.scrape_to_db(request.link)
                 website = webscraper.get_full_website()
                 text = ai_api.call_summarizer(short=request.short, policy=website, language_level=request.lang)
+
                 return convert_to_html(text)
             else:
-    
-                if request.query is not None:
 
+                if request.query is not None:
                     try:
                         text = ai_api.chat_completion(link=request.link, query=request.query)
                         return convert_to_html(text)
                     except NoResultFound:
                         webscraper.scrape_to_db(request.link)
                         text = ai_api.chat_completion(link=request.link, query=request.query)
-
                         return convert_to_html(text)
-    
-                else:
 
+                else:
                     raise HTTPException(status_code=400, detail="Query parameter cannot be empty for followup questions. Please provide a query in the request body.")
         else:
-
             raise HTTPException(status_code=429, detail="Too many requests. Please obey the 20s rule between requests")
-    except Exception as e:
-        return {"error": e}
+    else:
+        raise HTTPException(status_code=400, detail="Please provide a valid link")
 
 
 if __name__ == '__main__':
     uvicorn.run(app, port=800)
-
