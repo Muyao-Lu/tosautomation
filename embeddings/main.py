6e2601231ebe991fcb9737db7bd28bc3e3c2c3e3
Muyao Lu <130-MLU@sd43.bc.ca>
2025-08-02 13:18:09 -0700

    Fix: Changed app mode to

--- embeddings/main.py
+++ embeddings/main.py
@@ -6,7 +6,7 @@ import dotenv, requests
 import cohere
 dotenv.load_dotenv()
 
-APP_MODE = "testing"
+APP_MODE = "deployment"
 
 class Ranker:
 
@@ -115,4 +115,5 @@ async def process_ranking_request(request: RankerModel):
 if APP_MODE == "testing":
     if __name__ == "__main__":
         uvicorn.run(app, port=8000)
+
 
