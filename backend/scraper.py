7c7654f1e2980f5775523ba47f4d8dfa6c9f4f00
Muyao Lu <130-MLU@sd43.bc.ca>
2025-08-18 14:20:26 -0700

    Feat: PDF parsing for better user experience

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,6 +1,9 @@
 from langchain_hyperbrowser import HyperbrowserLoader
+from pdfminer.pdfparser import PDFSyntaxError
+
 from vector_db import vector_db
 import os, dotenv
+import requests, io, pdfplumber
 dotenv.load_dotenv()
 
 class ScraperModel:
@@ -25,7 +28,15 @@ class ScraperDatabaseControl:
 
     def scrape_to_db(self, link):
         self.document = self.scraper.scrape(link)
-        self.vector_db.add(link=link, item=str(self.document[0].page_content))
+        print("scraped", self.document[0].page_content)
+        if len(self.document[0].page_content) < 1:
+            print("pdf")
+            self.document = self.extract_pdf(link)
+            self.vector_db.add(link=link, item=self.document)
+        else:
+            self.vector_db.add(link=link, item=str(self.document[0].page_content))
+        print("added")
+
 
     def get_full_website(self):
         if self.document is not None:
@@ -38,3 +49,18 @@ class ScraperDatabaseControl:
             return self.vector_db.get_closest_neighbor(query)[0]
         else:
             return None
+
+    def extract_pdf(self, link):
+        d = ""
+        try:
+            headers = {'User-Agent': 'Mozilla/5.0'}
+            response = requests.get(link, headers=headers)
+            stream = io.BytesIO(response.content)
+            with pdfplumber.open(stream) as pdf:
+                for page in pdf.pages:
+                    d += page.extract_text()
+
+            return d
+        except PDFSyntaxError:
+            return ""
+
